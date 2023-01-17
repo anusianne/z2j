b686510970c24c883587a9b603ad5b6a57380407
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-01-17 20:33:52 +0100

    questions with confirm and prompt

--- f-102-l03.js
+++ f-102-l03.js
@@ -1,60 +1,14 @@
-let val = prompt("What is the official name of JS?", "");
-if (val === "ECMAScript") {
-  alert("That's correct!");
-} else {
-  alert("ECMAScript");
-}
-
-let num = prompt("Give me a number:", "");
-if (num > 0) {
-  alert(1);
-} else if (num < 0) {
-  alert(-1);
-} else {
-  alert(0);
-}
-
-a = 3;
-b = 2;
-let result = a + b < 4 ? "Below" : "Over";
-
-login = "Director";
-let message = console.log(
-  login == "Employee"
-    ? "Hello"
-    : login == "Director"
-    ? "Greetings"
-    : login == ""
-    ? "No login"
-    : ""
-);
-
-let hour = 12;
-let isWeekend = true;
-
-if (hour < 10 || hour > 18 || isWeekend) {
-  alert("The office is closed.");
-}
-
-if (age >= 14 && age <= 90) {
-  alert(age);
-}
-
-if (age < 14 && age > 90);
-if (!(age >= 14 && age <= 90));
-
-let login = prompt("Enter your login: ", "");
-if (login === "Admin") {
-  let password = prompt("Enter your password:", "");
-  if (password === "TheMaster") {
-    alert("Welcome!");
-  } else if (password === "" || password === null) {
-    alert("Wrong password.");
+let quest = confirm("Czy chcesz zostać Juniorem Frontendu? 💻");
+if (quest) {
+  alert("Idź wciąż naprzód i naprzód! 🔥");
+  let hours = prompt("Ile poświęcasz na naukę tygodniowo?", "");
+  if (hours <= 3) {
+    alert("Nie martw się, od jutra będzie lepiej!");
+  } else if (hours <= 7 && hours > 3) {
+    alert("Powoli do przodu!");
   } else {
-    alert("Canceled.");
+    alert("Tak trzymaj!");
   }
-} else if (login === "" || null) {
-  alert("Canceled.");
 } else {
-  alert("I do not know.");
+  alert("Wielka szkoda! Jakbyś zmienił zdanie, wróć do nas! ⚡");
 }
