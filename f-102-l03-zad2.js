a5115ba7b845732ee7c8d9c1fbd172d7c883cf3a
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-01-25 17:02:49 +0100

    if changed for functions

--- f-102-l03-zad2.js
+++ f-102-l03-zad2.js
@@ -1,3 +1,23 @@
+function addition(num1, num2) {
+  return num1 + num2;
+}
+function subtraction(num1, num2) {
+  return num1 - num2;
+}
+function multiplication(num1, num2) {
+  return num1 * num2;
+}
+function division(num1, num2) {
+  if (num2 === 0) {
+    alert("Nie dzieli się przez zero, spróbuj jeszcze raz!");
+    location.reload();
+  } else {
+    return num1 / num2;
+  }
+}
+function modulo(num1, num2) {
+  return num1 % num2;
+}
 let num1 = parseFloat(prompt("Wpisz pierwszą liczbę:", ""));
 while (isNaN(num1)) {
   alert("Wpisz liczbę.");
@@ -18,21 +38,4 @@ let num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
 if (isNaN(num2)) {
   alert("Wpisz liczbę.");
   num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
-}
-
-if (operation === "+") {
-  alert(num1 + num2);
-} else if (operation === "-") {
-  alert(num1 - num2);
-} else if (operation === "*") {
-  alert(num1 * num2);
-} else if (operation === "/") {
-  if (num2 === 0) {
-    alert("Nie dzieli się przez zero, spróbuj jeszcze raz!");
-    location.reload();
-  } else {
-    alert(num1 / num2);
-  }
-} else {
-  alert(num1 % num2);
 }
