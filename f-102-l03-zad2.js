876870adf77bae3014517f41f7c03f5e596f5ffa
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-01-26 14:53:32 +0100

    calculate fixed and finished

--- f-102-l03-zad2.js
+++ f-102-l03-zad2.js
@@ -11,62 +11,59 @@ function modulo(num1, num2) {
   return num1 % num2;
 }
 function division(num1, num2) {
-  if (num2 === 0) {
-    alert("Nie dzieli się przez zero, spróbuj jeszcze raz!");
-    location.reload();
+  return num1 / num2;
+}
+function calculate(ans) {
+  let num1;
+  if (ans === undefined) {
+    num1 = parseFloat(prompt("Wpisz pierwszą liczbę:", ""));
+    while (isNaN(num1)) {
+      alert("Wpisz liczbę.");
+      num1 = parseFloat(prompt("Wpisz pierwszą liczbę:", ""));
+    }
   } else {
-    return num1 / num2;
+    num1 = ans;
   }
+  let operator = prompt("Wybierz operator (+ ,- ,* ,/ albo %):", "");
+  while (
+    operator != "+" &&
+    operator != "-" &&
+    operator != "*" &&
+    operator != "/" &&
+    operator != "%"
+  ) {
+    alert("Wpisz poprawny znak działania.");
+    operator = prompt("Wybierz operator (+ ,- ,* ,/ albo %):", "");
+  }
+  let num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
+  if (isNaN(num2)) {
+    alert("Wpisz liczbę.");
+    num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
+  } else if (num2 === 0 && operator === "/") {
+    alert("Nie dzieli się przez zero, spróbuj jeszcze raz!");
+    num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
+  }
+  switch (operator) {
+    case "+":
+      result = addition(num1, num2);
+      break;
+    case "-":
+      result = subtraction(num1, num2);
+      break;
+    case "*":
+      result = multiplication(num1, num2);
+      break;
+    case "/":
+      result = division(num1, num2);
+      break;
+    case "%":
+      result = modulo(num1, num2);
+      break;
+    default:
+      alert("Błąd. Spróbuj ponownie i wpisz poprawny znak.");
+      break;
+  }
+  alert(result);
+  calculate(result);
 }
-function nextStep() {
-  operation = prompt(`Wynik to: ${result}, wybierz kolejny operator.`);
-  num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
-}
-let num1 = parseFloat(prompt("Wpisz pierwszą liczbę:", ""));
-while (isNaN(num1)) {
-  alert("Wpisz liczbę.");
-  num1 = parseFloat(prompt("Wpisz pierwszą liczbę:", ""));
-}
-let operation = prompt("Wybierz operator (+ ,- ,* ,/ albo %):", "");
-while (
-  operation != "+" &&
-  operation != "-" &&
-  operation != "*" &&
-  operation != "/" &&
-  operation != "%"
-) {
-  alert("Wpisz poprawny znak działania.");
-  operation = prompt("Wybierz operator (+ ,- ,* ,/ albo %):", "");
-}
-let num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
-if (isNaN(num2)) {
-  alert("Wpisz liczbę.");
-  num2 = parseFloat(prompt("Wpisz drugą liczbę:", ""));
-}
-let result;
-
-switch (operation) {
-  case "+":
-    result = addition(num1, num2);
-    nextStep();
-    break;
-  case "-":
-    result = subtraction(num1, num2);
-    nextStep();
-    break;
-  case "*":
-    result = multiplication(num1, num2);
-    nextStep();
-    break;
-  case "/":
-    result = division(num1, num2);
-    nextStep();
-    break;
-  case "%":
-    result = modulo(num1, num2);
-    nextStep();
-    break;
-  default:
-    alert("Błąd. Spróbuj ponownie i wpisz poprawny znak.");
-    break;
-}
+calculate();
