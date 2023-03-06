acc059f5ebe1fdaf53e9455c226a6c9b3b74214c
Anna Zielińska <ann.ka.zielinska@gmail.com>
2023-03-06 23:42:37 +0100

    add dom elements

--- f-103/f-103-l04.js
+++ f-103/f-103-l04.js
@@ -4,3 +4,6 @@ for (let i = 0; i < table.rows.length; i++) {
   let row = table.rows[i];
   row.cells[i].style.backgroundColor = "green";
 }
+let action = document.body;
+document.body.innerHTML = "Hello";
+action.innerHTML += "...";
