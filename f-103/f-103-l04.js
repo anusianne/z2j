5ec0d8798de9038c316c704b0a932d84ac7bc63c
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-03-08 21:14:03 +0100

    add switch

--- f-103/f-103-l04.js
+++ f-103/f-103-l04.js
@@ -1,3 +1,19 @@
 let div = document.createElement("div");
 div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
 document.body.append(div);
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert("Too small");
+    break;
+  case 4:
+    alert("Exactly!");
+    break;
+  case 5:
+    alert("Too big");
+    break;
+  default:
+    alert("I don't know such values");
+}
