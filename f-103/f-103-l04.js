302f29b7930f1722cb704ba1ff15ec7e3655e8bf
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-03-09 14:34:54 +0100

    add arrow function

--- f-103/f-103-l04.js
+++ f-103/f-103-l04.js
@@ -17,3 +17,17 @@ switch (a) {
   default:
     alert("I don't know such values");
 }
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+ask(
+  "Do you agree",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+
+
+
