c340c42cec93eb5beb6b6c9ab3b602628bd3da87
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-03-16 16:23:34 +0100

    add hide on click and toggle function

---
+++ f-103/f-103-l05.js
@@ -0,0 +1,15 @@
+function removalFunction() {
+  document.getElementById("text").style.display = "none";
+}
+
+function removalFunction1() {
+  document.getElementById("text2").remove();
+}
+function toggle() {
+  let elem = document.getElementById("text3");
+  if (elem.style.display === "none") {
+    elem.style.display = "block";
+  } else {
+    elem.style.display = "none";
+  }
+}
