d30829eb0a74ae2939ead28cf56671c351ecda9d
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-03-17 13:13:47 +0100

    add toggler

---
+++ f-103/f-103-l05-toggler.js
@@ -0,0 +1,6 @@
+let menuButton = document.getElementById("menuButton");
+let menu = document.getElementById("menuButton");
+
+menuButton.addEventListener("click", function () {
+  menu.classList.toggle("open");
+});
