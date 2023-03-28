0e55b08f24148ea9ed44c19d6d4787db17bbdabb
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-03-28 14:23:14 +0200

    add ctrlkey and click function

--- f-103/f-103-l05-selectable-list.js
+++ f-103/f-103-l05-selectable-list.js
@@ -1,5 +1,11 @@
-document
-  .querySelectorAll("li")
-  .forEach((li) =>
-    li.addEventListener("click", () => (li.style.backgroundColor = "red"))
-  );
+document.querySelectorAll("li").forEach((li) =>
+  li.addEventListener("click", (e) => {
+    if (li.hasAttribute("class", "selected")) {
+      if (e.ctrlKey)
+      li.removeAttribute("class", "selected");
+    } else {
+      li.setAttribute("class", "selected");
+    }
+  })
+);
+// document.querySelectorAll("li").forEach((li) => li.addEventListener("mousedown", () => li.style.backgroundColor = "white"));
