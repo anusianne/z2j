fd71bbb36ece5960a7483f6dd862738fc9967411
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-03-29 20:52:08 +0200

    add needed variables

---
+++ f-103/f-103-l06/f-103-egzamin.js
@@ -0,0 +1,13 @@
+const player1 = "X";
+const player2 = "O";
+const square = document.getElementsByClassName("square");
+const s11 = document.getElementById("s11");
+const s12 = document.getElementById("s12");
+const s13 = document.getElementById("s13");
+const s21 = document.getElementById("s21");
+const s22 = document.getElementById("s22");
+const s23 = document.getElementById("s23");
+const s31 = document.getElementById("s31");
+const s32 = document.getElementById("s32");
+const s33 = document.getElementById("s33");
+const arr = [s11, s12, s13, s21, s22, s23, s31, s32, s33];
