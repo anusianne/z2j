516e81cdf10f226f8ec6e93da70e9a8c97920c5d
Anna Zielinska <ann.ka.zielinska@gmail.com>
2023-04-19 18:42:21 +0200

    add validation and finished alert

--- f-105/f-105-l01.js
+++ f-105/f-105-l01.js
@@ -8,58 +8,54 @@
 // let newOpt = new Option("Classic", "classic");
 // genres.appendChild(newOpt);
 // newOpt.selected = true;
-function errorAlert(elem, msg) {
-  document.getElementById(elem).innerHTML = msg;
-}
-function validateForm() {
-    
-  console.log("dupa");
-  let fname = document.contactForm.fname.value;
-  let lname = document.contactForm.lname.value;
-  let sex = document.contactForm.sex.value;
-  let login = document.contactForm.login.value;
-  let pass = document.contactForm.pass.value;
-  let datebirth = document.contactForm.datebirth.value;
-  let fnameErr = true;
-  let lnameErr = true;
-  let sexErr = true;
-  let loginErr = true;
-  let passErr = true;
-  let datebirthErr = true;
-  console.log(fname);
 
-  if (fname === "") {
-    errorAlert("fnameErr", "Proszę wpisać imię.");
-  } else {
-    fnameErr = false;
-  }
-  if (lname === "") {
-    errorAlert("lnameErr", "Proszę wpisać nazwisko.");
-  } else {
-    lnameErr = false;
-  }
-  if (sex === "") {
-    errorAlert("sexErr", "Proszę zaznaczyć płeć.");
-  } else {
-    sexErr = false;
-  }
-  if (login === "") {
-    errorAlert("loginErr", "Proszę wpisać login.");
-  } else {
-    loginErr = false;
-  }
-  if (pass === "") {
-    errorAlert("passErr", "Proszę wpisać hasło.");
-  } else {
-    passErr = false;
-  }
-  if (datebirth === "") {
-    errorAlert("datebirthErr", "Proszę uzupełnić datę urodzenia.");
-  } else {
-    datebirthErr = false;
-  }
-  if (fnameErr || lnameErr || sexErr || loginErr || passErr || datebirthErr) {
-    //tutaj alert
-  } else {
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function validateForm() {
+  const formData = {
+    fname: document.getElementById("fname").value.trim(),
+    lname: document.getElementById("lname").value.trim(),
+    sex: document.querySelector('input[name="sex"]:checked'),
+    login: document.getElementById("login").value.trim(),
+    pass: document.getElementById("pass").value.trim(),
+    date: document.getElementById("datebirth").value,
+  };
+  for (let i in formData) {
+    if (formData[i] == "" || !formData.sex) {
+      document.getElementById(i + "Err").innerHTML = "Proszę wypełnić pole!";
+    } else {
+      document.getElementById(i + "Err").innerHTML = "";
+      if (formData.sex.value === "man") {
+        alert(
+          "Pan " +
+            formData.fname +
+            " " +
+            formData.lname +
+            " " +
+            " urodzony " +
+            formData.date +
+            " chce utworzyć konto o loginie " + "'" +
+            formData.login + "'"
+        );
+      } else if (formData.sex.value === "woman") {
+        alert(
+          "Pani " +
+            formData.fname +
+            " " +
+            formData.lname +
+            " " +
+            "urodzona" +
+            formData.date +
+            " chce utworzyć konto o loginie " +
+            "'" +
+            formData.login +
+            "'"
+        );
+      }
+      break;
+    }
   }
 }
