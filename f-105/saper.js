f9ea6444b607663575072c12535aa0c59f964010
unknown <ann.ka.zielinska@gmail.com>
2023-07-28 21:14:45 +0200

    add container class

--- f-105/saper.js
+++ f-105/saper.js
@@ -4,8 +4,7 @@ function levelBtnHandler() {
   console.log("hi");
   let newDiv = document.createElement("div");
   newDiv.className = "container";
-  newDiv.style.gridTemplateColumns = "repeat(8,1fr)";
-  newDiv.style.gridTemplateRows = "repeat(8,1fr)";
+
   document.body.appendChild(newDiv);
 }
 for (el of levelBtns) {
