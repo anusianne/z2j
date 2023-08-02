72413b52c8a6c2b88c577a8dffac811b5544ff26
unknown <ann.ka.zielinska@gmail.com>
2023-08-02 16:50:01 +0200

    added medium level box

--- f-105/saper.js
+++ f-105/saper.js
@@ -1,6 +1,7 @@
 const levelBtns = document.getElementsByClassName("level");
 const levelChooser = document.getElementById("levelChooser");
 const easy = document.getElementById("easy");
+const medium = document.getElementById("medium");
 easy.addEventListener("click", function () {
   let newDiv = document.createElement("div");
   levelChooser.style.display = "none";
@@ -9,6 +10,19 @@ easy.addEventListener("click", function () {
   for (let i = 1; i <= 64; i++) {
     let box = document.createElement("div");
     box.classList.add("box");
+    newDiv.appendChild(box);
+  }
+});
+medium.addEventListener("click", function () {
+  let newDiv = document.createElement("div");
+  levelChooser.style.display = "none";
+  newDiv.classList.add("container");
+  document.body.appendChild(newDiv);
+  for (let i = 1; i <= 256; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    newDiv.style.gridTemplateColumns = "repeat(16,50px)";
+    newDiv.style.gridTemplateRows = "repeat(16,50px)";
     newDiv.appendChild(box);
   }
 });
