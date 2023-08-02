f1ffe41b2f5634e99dbe75e38c74f55d48c9771b
unknown <ann.ka.zielinska@gmail.com>
2023-08-02 16:52:25 +0200

    added master level box

--- f-105/saper.js
+++ f-105/saper.js
@@ -2,6 +2,7 @@ const levelBtns = document.getElementsByClassName("level");
 const levelChooser = document.getElementById("levelChooser");
 const easy = document.getElementById("easy");
 const medium = document.getElementById("medium");
+const master = document.getElementById("master");
 easy.addEventListener("click", function () {
   let newDiv = document.createElement("div");
   levelChooser.style.display = "none";
@@ -22,6 +23,19 @@ medium.addEventListener("click", function () {
     let box = document.createElement("div");
     box.classList.add("box");
     newDiv.style.gridTemplateColumns = "repeat(16,50px)";
+    newDiv.style.gridTemplateRows = "repeat(16,50px)";
+    newDiv.appendChild(box);
+  }
+});
+master.addEventListener("click", function () {
+  let newDiv = document.createElement("div");
+  levelChooser.style.display = "none";
+  newDiv.classList.add("container");
+  document.body.appendChild(newDiv);
+  for (let i = 1; i <= 480; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    newDiv.style.gridTemplateColumns = "repeat(30,50px)";
     newDiv.style.gridTemplateRows = "repeat(16,50px)";
     newDiv.appendChild(box);
   }
