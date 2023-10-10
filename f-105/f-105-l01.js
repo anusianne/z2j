2c8e5ef28d4bab3aa753eab0db9e3a51311b70a0
unknown <ann.ka.zielinska@gmail.com>
2023-10-10 19:18:26 +0200

    added ladder object and this

--- f-105/f-105-l01.js
+++ f-105/f-105-l01.js
@@ -1,21 +1,43 @@
-let styles = ["Jazz", "Blues"];
-styles.push("Rock-n-Roll");
-console.log(styles);
-styles[1] = "Classics";
-console.log(styles);
-console.log(styles[0]);
-styles.shift();
-styles.unshift("Rap", "Reggae");
-console.log(styles);
-
+// let styles = ["Jazz", "Blues"];
+// styles.push("Rock-n-Roll");
+// console.log(styles);
+// styles[1] = "Classics";
+// console.log(styles);
+// console.log(styles[0]);
+// styles.shift();
+// styles.unshift("Rap", "Reggae");
+// console.log(styles);
 // ! calculator object with two methods:
-let calculator = {
-  sum() {
-    return this.a + this.b;
+// let calculator = {
+//   sum() {
+//     return this.a + this.b;
+//   },
+//   mul() {
+//     return this.a * this.b;
+//   },
+// };
+// alert(calculator.sum());
+// alert(calculator.mul());
+// ! ladder object that allows to go up and down:
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
   },
-  mul() {
-    return this.a * this.b;
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    // shows the current step
+    alert(this.step);
+    return this;
   },
 };
-alert(calculator.sum());
-alert(calculator.mul());
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.down();
+ladder.showStep(); // 0
