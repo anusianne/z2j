28bf4f25b7d17cef6261c115004b917ccc828652
unknown <ann.ka.zielinska@gmail.com>
2023-10-10 21:19:00 +0200

    added accumulator function

--- f-105/f-105-l01.js
+++ f-105/f-105-l01.js
@@ -35,21 +35,31 @@
 //   },
 // };
 // ladder.up().up().down().showStep().down().showStep();
-// ! construction function calculator
-function Calculator() {
+// // ! construction function calculator
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt("a?", 0);
+//     this.b = +prompt("b", 0);
+//   };
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+// }
+// let calculator = new Calculator();
+// calculator.read();
+// alert("Sum=" + calculator.sum());
+// alert("Mul=" + calculator.mul());
+//! constructor function Accumulator(starting value)
+function Accumulator(startingValue) {
+  this.value = startingValue;
   this.read = function () {
-    this.a = +prompt("a?", 0);
-    this.b = +prompt("b", 0);
-  };
-  this.sum = function () {
-    return this.a + this.b;
-  };
-  this.mul = function () {
-    return this.a * this.b;
+    this.value += +prompt("How much to add?", 0);
   };
 }
-let calculator = new Calculator();
-calculator.read();
-
-alert("Sum=" + calculator.sum());
-alert("Mul=" + calculator.mul());
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
