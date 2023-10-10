719a156ad701ad1f03792dbec1cdf7fcdf000e0c
unknown <ann.ka.zielinska@gmail.com>
2023-10-10 19:41:07 +0200

    added construction function calculator

--- f-105/f-105-l01.js
+++ f-105/f-105-l01.js
@@ -18,26 +18,38 @@
 // };
 // alert(calculator.sum());
 // alert(calculator.mul());
-// ! ladder object that allows to go up and down:
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function () {
-    // shows the current step
-    alert(this.step);
-    return this;
-  },
-};
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep(); // 1
-ladder.down();
-ladder.showStep(); // 0
+// // ! ladder object that allows to go up and down:
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return this;
+//   },
+//   down() {
+//     this.step--;
+//     return this;
+//   },
+//   showStep: function () {
+//     alert(this.step);
+//     return this;
+//   },
+// };
+// ladder.up().up().down().showStep().down().showStep();
+// ! construction function calculator
+function Calculator() {
+  this.read = function () {
+    this.a = +prompt("a?", 0);
+    this.b = +prompt("b", 0);
+  };
+  this.sum = function () {
+    return this.a + this.b;
+  };
+  this.mul = function () {
+    return this.a * this.b;
+  };
+}
+let calculator = new Calculator();
+calculator.read();
+
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());
