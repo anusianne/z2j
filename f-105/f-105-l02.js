ed5bcb3671c2f7d9b5fb91743383c8586038e9f7
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-01-12 22:12:05 +0100

    added exercise with number data

--- f-105/f-105-l02.js
+++ f-105/f-105-l02.js
@@ -58,9 +58,51 @@ function Car(make,model,year,color) {
     this.model = model;
     this.year = year;
     this.color = color;
+    this.drive = function() {
+        console.log(`You drive the ${this.model}`)
+    }
 }
 const car1 = new Car("Ford","Mustang",2024,"red");
 const car2 = new Car("Chevrolet","Camaro", 2018, "green");
 const car3 = new Car("Dodge", "Charger", 2016, "silver");
+car1.drive();
+car2.drive();
+car3.drive();
 
+//User Constructor
+function User(fName,lName,age,fMovie,fSport) {
+    this.firstName = fName;
+        this.lastName = lName;
+        this.age = age;
+        this.movie = fMovie;
+        this.sport = fSport;
+}
 
+//using User Conctructor
+
+let user = new User('Anna', 'Beck', 55, 'Interstellar', 'Swimming' );
+console.log(user);
+
+//let num = new Number();
+//Finding the minimum value:
+function min(arr) {
+    return Math.min(...arr);
+}
+min([1,2,3,4,100]);
+
+//Coding in function whatNumberIsIt. function accept 1 parameter:n. it's a number.To judge the number n. If n is one of the above five constants, return one of these string:
+function whatNumberIsIt(n){
+    if (n === Number.MAX_VALUE) {
+        return "Input number is Number.MAX_VALUE";
+    } else if (n === Number.MIN_VALUE) {
+        return "Input number is Number.MIN_VALUE";
+    } else if (isNaN(n)) {
+        return "Input number is Number.NaN";
+    } else if (n === Number.NEGATIVE_INFINITY) {
+        return "Input number is Number.NEGATIVE_INFINITY";
+    } else if (n === Number.POSITIVE_INFINITY) {
+        return "Input number is Number.POSITIVE_INFINITY";
+    } else {
+        return "Input number is " + n;
+    }
+}
