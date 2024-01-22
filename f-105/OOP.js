b3c4749e41e74436541127b2c1fcd072693c37ab
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-01-22 17:45:59 +0100

    added getter & setter for class

--- f-105/OOP.js
+++ f-105/OOP.js
@@ -25,6 +25,48 @@ class Square {
     get area() {
         return this.width * this.height;
     }
+    set area(area) {
+        this.width = Math.sqrt(area);
+        this.height = this.width;
+    }
 }
-let square1 = new Square(25);
-console.log(square1.area)
+let square1 = new Square(4);
+square1.area = 25;
+console.log(square1.width)
+
+// Getters (readeable) & setters (writeable)
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    set width(newWidth) {
+        if (newWidth > 0) {
+            this._width = newWidth;
+        }
+        else {
+            console.error("Width must be a positive number");
+        }
+    }
+    set height(newHeight) {
+        if (newHeight > 0) {
+            this._height = newHeight;
+        }
+        else {
+            console.error("Height must be a positive number")
+        }
+    }
+    get width() {
+        return this._width;
+    }
+    get height() {
+        return this._height;
+    }
+    get area() {
+        return this._width * this._height;
+    }
+}
+const rect = new Rectangle(3, 4);
+console.log(rect.width);
+console.log(rect.height)
+console.log(rect.area)
