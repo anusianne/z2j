1e6de47f79d14c777654f3559090dc8203cb77ba
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-01-27 20:33:20 +0100

    callback function replaced with promise

--- f-105/f-105-l05-promise.js
+++ f-105/f-105-l05-promise.js
@@ -23,3 +23,42 @@ let promise2 = new Promise((resolve,reject) => {
 promise2.catch(() => {
     alert("Rejected");
 });
+// Promises next
+function walkDog() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("You walk the dog.");
+        }, 1500);
+    });
+}
+function cleanKitchen() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("You clean the kitchen.");
+        }, 2500);
+    });
+}
+function takeOutTrash() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("You take out the trash.")
+        }, 500);
+    });
+}
+walkDog().then(value => {
+    console.log(value);
+    return cleanKitchen().then(value => {
+        console.log(value)
+        return takeOutTrash().then(value => {
+            console.log(value)
+        })
+    })
+})
+//solving with callbacks functions
+// walkDog(() => {
+//     cleanKitchen(() => {
+//         takeOutTrash(() => {
+//             console.log("You finished all the chores.");
+//         });
+//     });
+// });
