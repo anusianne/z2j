013ce0bf67d4cd37c2ba533dddd8afda8db2fd03
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-01-27 19:10:36 +0100

    added promise with resolve scenario and rejected scenario

--- f-105/f-105-l05-promise.js
+++ f-105/f-105-l05-promise.js
@@ -5,4 +5,21 @@ function delay(ms) {
     });
 }
 delay(3000).then(() => alert('runs after 3 seconds'));
-
+//Promise - resolve scenario
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Brought.');
+    }, 5000)
+})
+promise.then(() => {
+    alert("Its resolved succesfully!");
+});
+//Promise - rejected scenario
+let promise2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        reject("Not brought.");
+    }, 5000)
+});
+promise2.catch(() => {
+    alert("Rejected");
+});
