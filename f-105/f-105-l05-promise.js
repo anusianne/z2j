1aff1df33e3b35c3072abf3ae33bc7894058b095
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-01-27 20:48:24 +0100

    added promise all method

--- f-105/f-105-l05-promise.js
+++ f-105/f-105-l05-promise.js
@@ -62,3 +62,35 @@ walkDog().then(value => {
 //         });
 //     });
 // });
+
+//Promise with WebDev
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if (a === 2) {
+        resolve("Success.");
+    } else {
+        reject("Failed.");
+    }
+})
+p.then((message) => {
+    console.log("This is in the then " + message)
+}).catch((message) => {
+    console.log("This is in the catch " + message)
+});
+//Video Promises
+const recordVideoOne = new Promise((resolve,reject) => {
+    resolve("Video 1 Recorded.");
+})
+const recordedVideoTwo = new Promise((resolve, reject) => {
+    resolve("Video 2 Recorded.");
+})
+const recordVideoThree = new Promise((resolve, reject) => {
+    resolve("Video 3 Recorded.");
+})
+Promise.all([
+    recordVideoOne,
+    recordedVideoTwo,
+    recordVideoThree
+]).then((message) => {
+    console.log(message)
+})
