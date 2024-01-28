08a33a9fbae34072439dfff553ce7982927777f5
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-01-28 21:02:18 +0100

    added chaining promise

--- f-105/f-105-l05-promise.js
+++ f-105/f-105-l05-promise.js
@@ -94,3 +94,16 @@ Promise.all([
 ]).then((message) => {
     console.log(message)
 })
+// Promises chaining
+new Promise(function(resolve,reject) {
+    setTimeout(() =>
+        resolve(1), 1000);
+    }).then(function(result){
+        alert(result);
+        return result*2;
+    }).then(function(result){
+        alert(result);
+        return result*2;
+}).then(function(result){
+    alert(result);
+})
