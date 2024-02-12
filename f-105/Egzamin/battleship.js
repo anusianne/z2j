4bbcbc203b641fa82468a70f9e9e529029c7d080
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2024-02-12 18:38:48 +0100

    added basic board game

---
+++ f-105/Egzamin/battleship.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', createBoard);
+
+function createBoard() {
+    const div = document.createElement('div');
+    div.classList.add('container');
+    const shipDiv = document.createElement('div');
+    shipDiv.classList.add('shipContainer');
+    shipDiv.style.display = "grid";
+    shipDiv.style.gridTemplateColumns = "repeat(10, 1fr)";
+    shipDiv.style.gridTemplateRows = "repeat(10, 1fr)";
+
+    //adding grid cells
+    for (let i = 0; i<100; i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        shipDiv.appendChild(gridCell);
+    }
+
+    div.appendChild(shipDiv);
+    document.body.appendChild(div);
+
+
+}
