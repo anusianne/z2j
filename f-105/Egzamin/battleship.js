067dd2fdff224c61dc06b3a94e862bc647fd7534
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-02-13 21:57:17 +0100

    added createboards function

--- f-105/Egzamin/battleship.js
+++ f-105/Egzamin/battleship.js
@@ -1,23 +1,25 @@
-window.addEventListener('load', createBoard);
+window.addEventListener("load", createBoard);
 
+function createBoards() {
+  createBoard("playerBoard");
+  createBoard("aiBoard");
+}
 function createBoard() {
-    const div = document.createElement('div');
-    div.classList.add('container');
-    const shipDiv = document.createElement('div');
-    shipDiv.classList.add('shipContainer');
-    shipDiv.style.display = "grid";
-    shipDiv.style.gridTemplateColumns = "repeat(10, 1fr)";
-    shipDiv.style.gridTemplateRows = "repeat(10, 1fr)";
+  const div = document.createElement("div");
+  div.classList.add("container");
+  const shipDiv = document.createElement("div");
+  shipDiv.classList.add("shipContainer");
+  shipDiv.style.display = "grid";
+  shipDiv.style.gridTemplateColumns = "repeat(10, 1fr)";
+  shipDiv.style.gridTemplateRows = "repeat(10, 1fr)";
 
-    //adding grid cells
-    for (let i = 0; i<100; i++) {
-        const gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        shipDiv.appendChild(gridCell);
-    }
+  //adding grid cells
+  for (let i = 0; i < 100; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("grid-cell");
+    shipDiv.appendChild(gridCell);
+  }
 
-    div.appendChild(shipDiv);
-    document.body.appendChild(div);
-
-
+  div.appendChild(shipDiv);
+  document.body.appendChild(div);
 }
