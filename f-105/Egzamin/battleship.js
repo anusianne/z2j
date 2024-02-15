f50e61786f2b210d798e310d1fda29bdb96d3f6b
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-02-15 19:47:27 +0100

    added computer board

--- f-105/Egzamin/battleship.js
+++ f-105/Egzamin/battleship.js
@@ -1,25 +1,36 @@
-window.addEventListener("load", createBoard);
+window.addEventListener("load", createBoards);
 
 function createBoards() {
   createBoard("playerBoard");
   createBoard("aiBoard");
 }
-function createBoard() {
-  const div = document.createElement("div");
-  div.classList.add("container");
-  const shipDiv = document.createElement("div");
-  shipDiv.classList.add("shipContainer");
-  shipDiv.style.display = "grid";
-  shipDiv.style.gridTemplateColumns = "repeat(10, 1fr)";
-  shipDiv.style.gridTemplateRows = "repeat(10, 1fr)";
-
+function createBoard(boardId) {
+  const board = document.getElementById(boardId);
   //adding grid cells
   for (let i = 0; i < 100; i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("grid-cell");
-    shipDiv.appendChild(gridCell);
+    board.appendChild(gridCell);
   }
+  if (board === "aiBoard") {
+    board.addEventListener("click", aiMove);
+    placeComputerShips();
+  }
+}
 
-  div.appendChild(shipDiv);
-  document.body.appendChild(div);
+function aiMove(event) {
+  const computerShotIndex = Math.floor(Math.random() * 100);
+  const cell = event.target;
+  // Simulate hit or miss (for demonstration purposes)
+  if (cell.classList.contains("ship")) {
+    cell.style.backgroundColor = "red"; // Mark as hit
+  } else {
+    cell.style.backgroundColor = "blue";
+  }
+}
+function placeComputerShips() {
+  const aiBoard = document.getElementById("aiBoard");
+  const gridCells = document.querySelectorAll(".grid-cell");
+  const randomCellIndex = Math.floor(Math.random() * cells.length);
+  cells[randomCellIndex].classList.add("ship");
 }
