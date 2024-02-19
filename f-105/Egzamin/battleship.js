4d6da15b410249613e61e53c502f07276e0ec0de
Anna Zielińska <ann.ka.zielinska@gmail.com>
2024-02-19 19:54:47 +0100

    added fixed function addShip

--- f-105/Egzamin/battleship.js
+++ f-105/Egzamin/battleship.js
@@ -17,6 +17,7 @@ function createBoard(color, user) {
   const gameBoardContainer = document.createElement("div");
   gameBoardContainer.classList.add("gameBoard");
   gameBoardContainer.style.backgroundColor = color;
+  gameBoardContainer.id = user;
   //create 100 gridcells for each board
   for (let i = 1; i <= width * width; i++) {
     const gridCell = document.createElement("div");
@@ -31,3 +32,37 @@ function createBoards() {
   createBoard("yellow", "ai");
 }
 createBoards();
+// Ship class
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const battleship = new Ship("battleship", 4);
+const carrier = new Ship("carrier", 5);
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+//Adding randomly ship to an ai board
+function addShip(ship) {
+  const allBoardGrids = document.querySelectorAll("#ai div");
+  let randomStartIndex = Math.floor(Math.random() * width * width);
+  let randomBoolean = Math.random() < 0.5;
+  console.log(randomStartIndex);
+  let isHorizontal = randomBoolean;
+  let shipBlocks = [];
+  for (let i = 0; i < ship.length; i++) {
+    if (isHorizontal) {
+      shipBlocks.push(allBoardGrids[Number(randomStartIndex) + i]);
+    } else {
+      shipBlocks.push(allBoardGrids[Number(randomStartIndex) + i * width]);
+    }
+  }
+  shipBlocks.forEach((shipBlock) => {
+    shipBlock.classList.add(ship.name);
+    shipBlock.classList.add("taken");
+  });
+}
+addShip(destroyer);
